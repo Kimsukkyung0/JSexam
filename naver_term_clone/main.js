2b57736466bea6f360cdf071836ab8c1d239fa8c
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-10-06 22:13:55 +0900

    Create main.js

---
+++ naver_term_clone/main.js
@@ -0,0 +1,9 @@
+let subToggle=true;
+$(".language-dropdown").hover(()=>{
+  if(subToggle){
+    $("#language-content").slideDown(1000);
+  }else{
+    $("#language-content").slideUp(1000);
+  }
+  subToggle=!subToggle;
+});
