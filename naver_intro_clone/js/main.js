0984007073f2f968fe1ee783f546fcee0c5d6933
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-11-08 20:15:22 +0900

    Update main.js
    domcontent loaded를 통한 문서불러오기작업후 js코드 실행되도록 설정(Uncaught TypeError 해결)

--- naver_intro_clone/js/main.js
+++ naver_intro_clone/js/main.js
@@ -1,4 +1,7 @@
-//슬라이드 전체너비 구하기
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOMContentLoaded");
+
+    //슬라이드 전체너비 구하기
 const slide = document.querySelector(".slide_ctnt_list");
 let slideTotalWidth = slide.clientWidth;
 
@@ -16,30 +19,18 @@ const maxPage = slideItems.length;
 console.log(maxPage);
 
 //페이지네이션이라는 개념(index에 ul태그 추가후 html 소스 주입방식인듯)
-const pagination = document.querySelector("slide_pagination");
+const pagination = document.querySelector(".slide_pagination");
+
+for(let i=0; i<maxPage; i++){
+    if(i===0){
+        pagination.innerHTML += `<li class="active"></li>`;
+    }else{
+        pagination.innerHTML +=`<li></li>`;
+    }
+}
+//페이지네이션내부 아이템 선택자
+
+const paginationItems = document.querySelectorAll(".slide_pagination > li");
+});
 
 
-
-//   //JQUERY 로 작성된 네이버 공홈 문법
-var mainSwiper = new Swiper('.slide_container', {
-//         initialSlide: startIdx,
-//         effect: 'slide', // 'slide'(default), 'fade'
-//         autoplay: {
-//             delay: 5000,
-//             disableOnInteraction: false, //사용자 상호 작용 후 자동 재생이 중지되지 않으며 매회 다시 시작
-//         },
-//         speed: 1000,
-//         fadeEffect: {
-//             crossFade: true, //이펙트가 'fade'일 경우
-//         },
-//         loop: true,
-//         setWrapperSize: true, //모든 슬라이드의 총 너비/높이 설정
-//         navigation: {
-//             prevEl: '.btn_prev',
-//             nextEl: '.btn_next',
-//         },
-//         threshold: 300, // 임계값이 300이상일 때만 스와이퍼 작동
-//     });
-
-//     showSlideInfo(startIdx);
-//     mainSwiper.autoplay.start(); //자동 재생 시작
