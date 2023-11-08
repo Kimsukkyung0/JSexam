1a9c70e934f6f763543a5fffb13f5eadab07940b
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-11-08 20:33:31 +0900

    페이지네이션

--- naver_intro_clone/js/main.js
+++ naver_intro_clone/js/main.js
@@ -31,6 +31,57 @@ for(let i=0; i<maxPage; i++){
 //페이지네이션내부 아이템 선택자
 
 const paginationItems = document.querySelectorAll(".slide_pagination > li");
+//list 아이템즈가 정확히 주가됐는지 확인가넝.
+console.log(paginationItems);
+
+
+
+
+//여기서부터 모르겠다.
+nextBtn.addEventListener("click",() => {
+    currentSlide++;
+
+    if(currentSlide ==maxSlide){
+        const offset = slideTotalWidth * (currentSlide - 1);
+        slideItems.forEach((i) => {
+            i.setAttribute("style", `left: ${-offset}px`);
+          });
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currentSlide - 1].classList.add("active");
+    } else {
+        currentSlide--;
+      }
 });
+
+// 버튼 엘리먼트에 클릭 이벤트 추가하기
+prevBtn.addEventListener("click", () => {
+    // 이전 버튼 누를 경우 현재 슬라이드를 변경
+    currentSlide--;
+    // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
+    if (currentSlide > 0) {
+      // 슬라이드를 이동시키기 위한 offset 계산
+      const offset = slideWidth * (currentSlide - 1);
+      // 각 슬라이드 아이템의 left에 offset 적용
+      slideItems.forEach((i) => {
+        i.setAttribute("style", `left: ${-offset}px`);
+      });
+      // 슬라이드 이동 시 현재 활성화된 pagination 변경
+      paginationItems.forEach((i) => i.classList.remove("active"));
+      paginationItems[currentSlide - 1].classList.add("active");
+    } else {
+      currentSlide++;
+    }
+  });
+  
+  // 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
+  window.addEventListener("resize", () => {
+    slideTotalWidth = slide.clientWidth;
+  });
+
+
+
+});
+
+
 
 
